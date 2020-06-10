cff6b73771cc3217f4f46460a9e00b21df1ca83d
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-06-10 12:52:44 -0700

    import/export pages

---
+++ src/components/pages/CreateAnswer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CreateAnswer() {
+  return <h1>Create Answer</h1>;
+}
---
+++ src/components/pages/CreateImagery.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CreateImagery() {
+  return <h1>Create Imagery</h1>;
+}
