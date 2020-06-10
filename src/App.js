ad966e70ad8eb353136e1cfc1f9f5427cb9d458b
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-06-10 12:37:42 -0700

    add page components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./style/master.scss"; // global styles
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-info">suh dude</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/create-answer" component={CreateAnswer} />
+        <Route exact path="/create-imagery" component={CreateImagery} />
+        <Route exact path="/review-imagery" component={ReviewImagery} />
+        <Route exact path="/review-answer" component={ReviewAnswer} />
+        <Route exact path="/review-empty" component={ReviewEmpty} />
+        <Route exact path="/all-cards" component={AllCards} />
+        <Route exact path="/edit" component={Edit} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
