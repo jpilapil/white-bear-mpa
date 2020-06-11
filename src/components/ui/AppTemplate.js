0d46ec4166f05b3f3b1c1375636960367784ab04
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-06-11 10:41:18 -0700

    add nav and header for pages

--- src/components/ui/AppTemplate.js
+++ src/components/ui/AppTemplate.js
@@ -5,6 +5,7 @@ export default function AppTemplate(props) {
     <div className="container">
       <div className="row mt-1">
         <div className="col-12 col-xl-6 offset-xl-3 col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+          {/* displays everything passed into AppTemplate component */}
           {props.children}
         </div>
       </div>
--- src/components/ui/Navigation.js
+++ src/components/ui/Navigation.js
@@ -1,30 +1,63 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
+  const url = window.location.pathname;
+  function tabActiveOnCreate(url) {
+    if (url.indexOf("create-imagery") > 0 || url.indexOf("create-answer") > 0) {
+      return "tab-active";
+    } else return "";
+  }
+
+  function tabActiveOnReviw(url) {
+    if (
+      url.indexOf("review-imagery") > 0 ||
+      url.indexOf("review-answer") > 0 ||
+      url.indexOf("review-empty") > 0
+    ) {
+      return "tab-active";
+    } else return "";
+  }
+
+  function tabActiveOnAllCards(url) {
+    if (url.indexOf("all-cards") > 0) {
+      return "tab-active";
+    } else return "";
+  }
+
   return (
     <div
       className="btn-group d-flex "
       role="navigation"
       aria-label="navigation"
     >
-      <button
-        href="create-answer.html"
-        className="btn btn-secondary navigation-button tab-separator"
+      <Link
+        to="/create-answer"
+        type="button"
+        className={`btn btn-secondary navigation-button tab-separator ${tabActiveOnCreate(
+          url
+        )}`}
       >
         Create new
-      </button>
-      <button
-        href="review-imagery.html"
-        className="btn btn-secondary navigation-button tab-separator"
+      </Link>
+      <Link
+        to="/review-imagery"
+        type="button"
+        className={`btn btn-secondary navigation-button tab-separator ${tabActiveOnReviw(
+          url
+        )}`}
       >
         Review
-      </button>
-      <button
-        href="/all-cards.html"
-        className="btn btn-secondary navigation-button tab-separator"
+      </Link>
+      <Link
+        to="/all-cards"
+        type="button"
+        className={`btn btn-secondary navigation-button tab-separator ${tabActiveOnAllCards(
+          url
+        )}`}
       >
         All cards
-      </button>
+      </Link>
     </div>
   );
 }
