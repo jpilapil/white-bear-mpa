6f702dff2c87c9d1bc4d423182ba3a68ef6f7915
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-06-28 14:29:02 -0700

    create json for memory cards and isntall axios

--- src/components/pages/ReviewImagery.js
+++ src/components/pages/ReviewImagery.js
@@ -1,31 +1,52 @@
 import React from "react";
 import AppTemplate from "../ui/AppTemplate";
 import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import memoryCards from "../../mock-data/memory-cards";
+import axios from "axios";
 
 const memoryCard = memoryCards[2];
 
-export default function ReviewImagery() {
-  return (
-    <AppTemplate>
-      <div>
-        <div className="mb-5 mt-3">
-          <div className="card bg-primary">
-            <div className="card-body">{memoryCard.imagery}</div>
+class ReviewImagery extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Make a request for a user with a given ID
+    axios
+      .get("https://run.mocky.io/v3/91873f97-6f2f-48d4-be40-bd12d67089fc")
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <AppTemplate>
+        <div>
+          <div className="mb-5 mt-3">
+            <div className="card bg-primary">
+              <div className="card-body">{memoryCard.imagery}</div>
+            </div>
+          </div>
+          <Link to="/all-cards" className="btn btn-link mt-2">
+            Previous card
+          </Link>
+          <div className="float-right mt-2 ml-5">
+            <Link
+              to="/review-answer"
+              className="btn btn-lg btn-outline-primary mr-0"
+            >
+              Show answer
+            </Link>
           </div>
         </div>
-        <Link to="/all-cards" className="btn btn-link mt-2">
-          Previous card
-        </Link>
-        <div className="float-right mt-2 ml-5">
-          <Link
-            to="/review-answer"
-            className="btn btn-lg btn-outline-primary mr-0"
-          >
-            Show answer
-          </Link>
-        </div>
-      </div>
-    </AppTemplate>
-  );
+      </AppTemplate>
+    );
+  }
 }
+export default withRouter(ReviewImagery);
