4bb2eda389cf87d004dcc0c0c6b32740176b992d
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-06-30 14:19:30 -0700

    add comments

--- src/store/reducers/queue.js
+++ src/store/reducers/queue.js
@@ -1,15 +1,17 @@
 import actions from "../actions";
 
 export default function queue(queue = {}, action) {
+  // initial state, action
   let newQueue = { ...queue }; // create copy of queue, never mutate original state!
   switch (action.type) {
-    case actions.STORE_QUEUED_CARDS:
+    case actions.STORE_QUEUED_CARDS: // if action is type of STORE_QUEUED_CARDS, run
       newQueue.cards = action.payload;
       return newQueue; // new state
-    case actions.UPDATE_INDEX_OF_CURRENT_CARD:
+    case actions.UPDATE_INDEX_OF_CURRENT_CARD: // if action is type of UPDATE_INDEX_OF_CURRENT_CARD, run
       newQueue.index += 1;
       return newQueue; // new state
     default:
+      // else, return initial state
       return queue;
   }
 }
